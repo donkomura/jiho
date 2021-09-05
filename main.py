b4bd4ca1ab4c076aaa6d11957e6d75642ca953e7
donkomura <koiru3822fs@gmail.com>
2021-09-05 13:37:53 +0900

    modify script

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ from pydub.playback import play
 audio_dir = "audio"
 intro = "intro.wav"
 
+
 def play_wav(path):
     sound = AudioSegment.from_file(path, format="wav")
     play(sound)
 
+
 def main():
-    current = os.getcwd()
-    now = datetime.datetime.now()
+    current = os.path.dirname(__file__)
+    audio_path = os.path.join(current, audio_dir)
 
     # intro
-    path_intro = current + "/" + audio_dir + "/" + intro
+    path_intro = os.path.join(audio_path, intro)
     play_wav(path_intro)
 
     # jiho
-    path_jiho = current + "/" + audio_dir + "/" + str(now.hour) + ".wav"
-    print("jiho: {}", path_jiho)
+    now = datetime.datetime.now()
+    path_jiho = os.path.join(audio_path, str(now.hour) + ".wav")
+    print("jiho: {} {}", path_jiho, now)
     play_wav(path_jiho)
+
 
 if __name__ == '__main__':
     main()
